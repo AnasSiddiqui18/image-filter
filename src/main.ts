3d7bfc787ffd587420650062306f61212915dbb2
Anas Siddiqui <anassiddiqui0910@gmail.com>
2024-05-24 14:42:02 +0500

    removed unused imports

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 const buttonParent = document.querySelector("#button_parent");
-const allButton = document.querySelector("#all-btn");
-const animalButton = document.querySelector("#animal-btn");
-const birdButton = document.querySelector("#bird-btn");
-const natureButton = document.querySelector("#nature-btn");
+const images = document.querySelectorAll("img");
 
 function filterImages(category: string) {
-  const images = document.querySelectorAll("img");
   images.forEach((img) => {
     if (category === "all" || img.dataset.category === category) {
       img.classList.remove("hidden");
